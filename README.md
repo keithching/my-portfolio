215c7d827920804a1a57d06c583924f36a6f8c6b
Keith <keithching91@gmail.com>
2022-06-02 22:11:59 +0900

    feat: UI improvements

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 This is my portfolio project.
 
 ## goals
-- serves as demonstration of my web dev experience
+- serves as my portfolio page.
+- also for demonstration of my web dev experience
 
 ## features
 - accessibility
@@ -11,5 +12,23 @@ This is my portfolio project.
     - media queries
     - CSS grid and flexbox
 
+## details
+1. media queries
+    - for smaller screen sizes, some stylings will be different, for example:
+        - narrower gaps between elements
+        - smaller font sizes
+        - some longer descriptions hidden
+        - smaller card sizes
+        - grid layout switched from column to row
+2. CSS grid and flexbox
+    - used grid for responsive card layout
+    - used flexbox for positioning elements
+
+## enhancement
+- TBD
+
+---
 ## references
 - https://emojiterra.com/beer-mug/
+- https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+- https://www.youtube.com/watch?v=RsPWEmfOQdU
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// credit and modal event listeners and DOM manipulations
 const creditsDiv = document.getElementById('credits');
 creditsDiv.addEventListener('click', () => {
     document.body.classList.add('stop-scrolling');
@@ -11,4 +12,26 @@ closeModalDiv.addEventListener('click', () => {
     const modalDiv = document.querySelector('.modal');
     modalDiv.classList.remove('show');
     document.body.classList.remove('stop-scrolling');
+});
+
+// nav event listeners and DOM manipulations
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= sectionTop - 30) { // get into the section this amount of pixels beforehand (a personal touch)
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+            li.classList.add('active');
+        }
+    });
 });
