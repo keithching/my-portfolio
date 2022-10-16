8aaab7be8b1d8516c49d9368abf12048892cbf62
Keith <keithching91@gmail.com>
2022-10-16 18:43:52 +0900

    feat: overall updates per latest resume

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // credit and modal event listeners and DOM manipulations
-const creditsDiv = document.getElementById('credits');
-creditsDiv.addEventListener('click', () => {
-    document.body.classList.add('stop-scrolling');
+// const creditsDiv = document.getElementById('credits');
+// creditsDiv.addEventListener('click', () => {
+//     document.body.classList.add('stop-scrolling');
 
-    const modalDiv = document.querySelector('.modal');
-    modalDiv.classList.add('show');
-});
+//     const modalDiv = document.querySelector('.modal');
+//     modalDiv.classList.add('show');
+// });
 
-const closeModalDiv = document.getElementById('close-modal');
-closeModalDiv.addEventListener('click', () => {
-    const modalDiv = document.querySelector('.modal');
-    modalDiv.classList.remove('show');
-    document.body.classList.remove('stop-scrolling');
-});
+// const closeModalDiv = document.getElementById('close-modal');
+// closeModalDiv.addEventListener('click', () => {
+//     const modalDiv = document.querySelector('.modal');
+//     modalDiv.classList.remove('show');
+//     document.body.classList.remove('stop-scrolling');
+// });
 
 // nav event listeners and DOM manipulations
 const sections = document.querySelectorAll('section');
@@ -20,10 +20,15 @@ const navLi = document.querySelectorAll('nav ul li');
 
 window.addEventListener('scroll', () => {
     let current = '';
+
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if (scrollY >= sectionTop - 30) { // get into the section this amount of pixels beforehand (a personal touch)
+
+        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 100) { // get into the section this amount of pixels beforehand (a personal touch)
+            current = 'contact';
+        }
+        else if (scrollY >= sectionTop - 30) { // get into the section this amount of pixels beforehand (a personal touch)
             current = section.getAttribute('id');
         }
     });
