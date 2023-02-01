20e159f79c9d5ce1f0d8739e6bf2ffccc7bf54e4
Keith <keithching91@gmail.com>
2023-02-01 20:35:44 +0900

    chore: css and js tidy up

--- script.js
+++ script.js
@@ -1,55 +1,44 @@
-// credit and modal event listeners and DOM manipulations
-// const creditsDiv = document.getElementById('credits');
-// creditsDiv.addEventListener('click', () => {
-//     document.body.classList.add('stop-scrolling');
-
-//     const modalDiv = document.querySelector('.modal');
-//     modalDiv.classList.add('show');
-// });
-
-// const closeModalDiv = document.getElementById('close-modal');
-// closeModalDiv.addEventListener('click', () => {
-//     const modalDiv = document.querySelector('.modal');
-//     modalDiv.classList.remove('show');
-//     document.body.classList.remove('stop-scrolling');
-// });
-
 // nav event listeners and DOM manipulations
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('nav ul li');
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
 
-window.addEventListener('scroll', () => {
-    let current = '';
+window.addEventListener("scroll", () => {
+  let current = "";
 
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
 
-        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 100) { // get into the section this amount of pixels beforehand (a personal touch)
-            current = 'contact';
-        }
-        else if (scrollY >= sectionTop - 30) { // get into the section this amount of pixels beforehand (a personal touch)
-            current = section.getAttribute('id');
-        }
-    });
+    if (
+      window.innerHeight + window.scrollY >=
+      document.body.scrollHeight - 100
+    ) {
+      // get into the section this amount of pixels beforehand (a personal touch)
+      current = "contact";
+    } else if (scrollY >= sectionTop - 30) {
+      // get into the section this amount of pixels beforehand (a personal touch)
+      current = section.getAttribute("id");
+    }
+  });
 
-    navLi.forEach(li => {
-        li.classList.remove('active');
-        if (li.classList.contains(current)) {
-            li.classList.add('active');
-        }
-    });
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
 });
 
 // back to top button event listener
-const backToTopBtn = document.getElementById('back-to-top-button');
+const backToTopBtn = document.getElementById("back-to-top-button");
 
 function handleScroll() {
-    const skillsDiv = document.getElementById('skills');
-    const footerDiv = document.querySelector('.footer');
-    if (scrollY >= skillsDiv.offsetTop / 4) { // personal touch
-        backToTopBtn.style.display = 'block';
-    } else backToTopBtn.style.display = 'none';
+  const skillsDiv = document.getElementById("skills");
+  const footerDiv = document.querySelector(".footer");
+  if (scrollY >= skillsDiv.offsetTop / 4) {
+    // personal touch
+    backToTopBtn.style.display = "block";
+  } else backToTopBtn.style.display = "none";
 }
 
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
